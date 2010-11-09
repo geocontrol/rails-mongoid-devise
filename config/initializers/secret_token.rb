d9372ea2d85500581daf6bfb3ffbb02b1c0df394
Mark Simpkins <mark@maahee.com>
2010-11-09 22:10:11 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMongoidDevise::Application.config.secret_token = '57b93c5aef4cb3a60c44d611a9619f18bd34db7d1becf45065223d650d86ac3e06b2adfaf3510881caa212b703d9fe4c82c0e63ff90ecac0da3faf3cdccc4272'
